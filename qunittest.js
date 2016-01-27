34fc008aa480127d3c4d2a19048f3c75e6b95062
tu6619 <tu6619@my.bristol.ac.uk>
2016-01-27 13:29:19 +0000

    First upload of files

--- qunittest.js
+++ qunittest.js
@@ -1,31 +1,3 @@
-function isEven(val) {
-    return val % 2 === 0;
-}
-
-function half(val){
-    return val /2;
-}
-
-function isMultiple3(val){
-    return val%3===0;
-}
-
-test("Name of the function1",function(){
-    ok(isEven(2),"this is correct");
+test("Starts from 00:00:00:00",function(){
+    equal(totalTime, 0, "timer should start at 0");
 });
-
-test("Name of the function2",function(){
-    ok(isEven(1),"this is correct");
-});
-
-test("Name of the function3", function(){
-    equal(isEven(2), true, "this is correct")
-});
-
-test("Name of the function4", function(){
-    assert.equal(isEven(1), true, "this is correct")
-});
-
-test("Name of the function5", function(){
-    asser
-}
